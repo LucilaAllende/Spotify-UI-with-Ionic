f006f21c66c7562f6447bcf4dcb4ab98ae57dca8
LucilaAllende <abigail_lucii@hotmail.com>
2024-01-23 09:43:37 -0300

    Overview and horizontal scroll

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import recentlyPlayed from '../../assets/data/recentlyPlayed.json';
+import heavyRotation from '../../assets/data/heavyRotation.json';
+import jumpBackIn from '../../assets/data/jumpBackIn.json';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +11,32 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  data = [
+    {
+      title: 'Recently played',
+      albums: recentlyPlayed
+    },
+    {
+      title: 'Heavy rotation',
+      albums: heavyRotation
+    },
+    {
+      title: 'Jump back in',
+      albums: jumpBackIn
+    },
+  ]
+
   constructor() {}
+
+  // Helper function for images names
+  dasherize(str: string) {
+    return str.replace(/[A-Z]/g, function (char, index) {
+      return (index!==0 ? '-': '') + char.toLowerCase();
+    })
+  }
+
+  openAlbum(album: { id: number; image: string; title: string; }) {
+    console.log(album);
+  }
 
 }
